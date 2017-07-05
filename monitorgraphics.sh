e92861998ef396e6fc7eba4755a694c7f713b296
Richard Kavanagh <r_kav_0000@hotmail.com>
2017-07-05 16:40:52 +0100

    Updating the sample script for monitoring
    Adjusting the monitoring sample script to cope with:
    * hosts with multiple GPUs
    * listing out pstate information
    * listing out throttling reasons

--- monitorgraphics.sh
+++ monitorgraphics.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Note: using the parameter -f ./GPU-stats.csv forces the script to run in the foreground. Redirecting standard out i.e. >> ./GPU-stats.csv does not have this limitation.
-nvidia-smi --query-gpu=timestamp,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used,power.draw,power.limit,fan.speed,temperature.gpu,compute_mode,clocks.current.graphics,clocks.current.sm,clocks.current.memory,clocks.current.video --format=csv,nounits --gpu=0 -l 1 >> ./GPU-stats.csv
+nvidia-smi --query-gpu=timestamp,index,name,uuid,serial,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used,power.draw,power.limit,fan.speed,temperature.gpu,compute_mode,clocks.current.graphics,clocks.current.sm,clocks.current.memory,clocks.current.video,gpu_operation_mode.current,pstate,clocks_throttle_reasons.active,clocks_throttle_reasons.gpu_idle,clocks_throttle_reasons.applications_clocks_setting,clocks_throttle_reasons.sw_power_cap,clocks_throttle_reasons.hw_slowdown,clocks_throttle_reasons.sync_boost,clocks_throttle_reasons.unknown --format=csv,nounits -l 1 >> ./$HOSTNAME-GPU-stats.csv
 
 
 
